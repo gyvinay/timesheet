8498acb9e36eb9689627f23d4eaa6c727c3ec97a
Vinay G Y <vinay.vv29@gmail.com>
2024-09-26 10:53:30 +0530

    init timesheet

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Scheduler } from "./lib";
+import { EVENTS } from "./events";
+import { useRef } from "react";
+import { SchedulerRef } from "./lib/types";
+
+function App() {
+  const calendarRef = useRef<SchedulerRef>(null);
+
+  return (
+    <Scheduler
+      ref={calendarRef}
+      events={EVENTS}
+      // events={generateRandomEvents(200)}
+    />
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={createTheme()}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
